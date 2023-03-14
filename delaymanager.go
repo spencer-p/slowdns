d6a59549cc803dab7b88b4e668f88629e3ea5b1c
Spencer Peterson <spencerjpeterson@gmail.com>
2023-03-14 12:52:19 -0700

    Don't separate timers by subdomains

--- delaymanager.go
+++ delaymanager.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -31,6 +32,14 @@ func (m *multiDelayManager) NextTimer(key string) *time.Timer {
 	}
 	if m.subManagers == nil {
 		m.subManagers = make(map[string]*delayManager)
+	}
+
+	// If we have requests for a.foo.com and b.foo.com, time them both on
+	// foo.com. But if we're getting DNS lookups for something without a TLD
+	// (intranet?) then just use the full key.
+	components := strings.Split(key, ".")
+	if n := len(components); n >= 2 {
+		key = components[n-2] + components[n-1]
 	}
 
 	subManager, ok := m.subManagers[key]
